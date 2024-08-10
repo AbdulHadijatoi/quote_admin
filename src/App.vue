09fbe7e00171c62b3a358f55958a75373002b92f
Abdul Hadi <Abdulhadijatoi@gmail.com>
2024-08-10 13:27:54 +0400

    updated styles

--- src/App.vue
+++ src/App.vue
@@ -5,3 +5,10 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 </script>
+
+<style>
+.v-field--variant-filled .v-field__overlay{
+  background: none;
+  border-radius: 3px;
+}
+</style>
